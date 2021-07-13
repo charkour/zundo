1f97106147309454d0982fd88f5fbb93a7073ebe
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2021-07-12 20:22:57 -0400

    pass options parameter through create

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ export { UndoState, undoMiddleware } from './middleware';
 export { UseStore } from 'zustand';
 
 import createStore, { State, StateCreator } from 'zustand';
-import undoMiddleware from './middleware';
+import undoMiddleware, { Options } from './middleware';
 
 // create a store with undo/redo functionality
-export const create = <TState extends State>(config: StateCreator<TState>) =>
-  createStore<TState>(undoMiddleware(config));
+export const create = <TState extends State>(
+  config: StateCreator<TState>,
+  options?: Options
+) => createStore<TState>(undoMiddleware(config, options));
 
 export default create;
