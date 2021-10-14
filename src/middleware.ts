8a86ed8c41447200fe20d5ba46c3608805266b82
Alex Kempton <alex@funwithtriangles.net>
2021-10-14 13:06:04 +0200

    Only storing changes if states aren't equal

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { GetState, SetState, StateCreator, StoreApi } from 'zustand/vanilla';
 import { createUndoStore, UndoStoreState } from './factory';
 import { filterState } from './utils';
+import isEqual from 'lodash/isEqual';
 
 export type UndoState = Partial<
   Pick<UndoStoreState, 'undo' | 'redo' | 'clear'> & {
@@ -11,6 +12,7 @@ export type UndoState = Partial<
 export interface Options {
   // TODO: improve this type. ignored should only be fields on TState
   omit?: string[];
+  allowUnchanged?: boolean;
 }
 
 // custom zustand middleware to get previous state
@@ -22,19 +24,13 @@ export const undoMiddleware = <TState extends UndoState>(
   const { getState, setState } = undoStore;
   return config(
     args => {
-      setState({
-        prevStates: [
-          ...getState().prevStates,
-          filterState({ ...get() }, options?.omit || []),
-        ],
-        setStore: set,
-        futureStates: [],
-        getStore: get,
-        options,
-      });
       /* TODO: const, should call this function and inject the values once, but it does
         it on every action call currently. */
       const { undo, clear, redo } = getState();
+      const prevStates = getState().prevStates;
+
+      // Get the last state before updating state
+      const lastState = filterState({ ...get() }, options?.omit || []);
 
       // inject helper functions to user defined store.
       set({
@@ -45,6 +41,23 @@ export const undoMiddleware = <TState extends UndoState>(
       });
 
       set(args);
+
+      // Get the current state after updating state
+      const currState = filterState({ ...get() }, options?.omit || []);
+      
+      // Only store changes if state isn't equal (or option has been set)
+      const shouldStoreChange =
+        !isEqual(lastState, currState) || options?.allowUnchanged;
+
+      if (shouldStoreChange) {
+        setState({
+          prevStates: [...prevStates, lastState],
+          setStore: set,
+          futureStates: [],
+          getStore: get,
+          options,
+        });
+      }
     },
     get,
     api
