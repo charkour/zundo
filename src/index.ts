46059d71d9f294216ab2cb5df634df7d762dee85
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2021-10-17 22:29:45 -0400

    switch import/export order

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-export { UndoState, undoMiddleware } from './middleware';
-export { UseStore } from 'zustand';
-
 import createStore, { State, StateCreator } from 'zustand';
 import undoMiddleware, { Options } from './middleware';
+export { UndoState, undoMiddleware } from './middleware';
+export { UseStore } from 'zustand';
 
 // create a store with undo/redo functionality
 export const create = <TState extends State>(
