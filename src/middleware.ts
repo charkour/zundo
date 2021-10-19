ca845ede8c1692a8b722f07098cc3fcaa18eb4dd
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2021-10-18 21:16:17 -0400

    fix linting errors

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,51 +16,52 @@ export interface Options {
 }
 
 // custom zustand middleware to get previous state
-export const undoMiddleware =
-  <TState extends UndoState>(config: StateCreator<TState>, options?: Options) =>
-  (set: SetState<TState>, get: GetState<TState>, api: StoreApi<TState>) => {
-    const undoStore = createUndoStore();
-    const { getState, setState } = undoStore;
-    return config(
-      (args) => {
-        /* TODO: const, should call this function and inject the values once, but it does
+export const undoMiddleware = <TState extends UndoState>(
+  config: StateCreator<TState>,
+  options?: Options
+) => (set: SetState<TState>, get: GetState<TState>, api: StoreApi<TState>) => {
+  const undoStore = createUndoStore();
+  const { getState, setState } = undoStore;
+  return config(
+    args => {
+      /* TODO: const, should call this function and inject the values once, but it does
       it on every action call currently. */
-        const { undo, clear, redo } = getState();
-        // inject helper functions to user defined store.
-        set({
-          undo,
-          clear,
-          redo,
-          getState,
+      const { undo, clear, redo } = getState();
+      // inject helper functions to user defined store.
+      set({
+        undo,
+        clear,
+        redo,
+        getState,
+      });
+
+      // Get the last state before updating state
+      const lastState = filterState({ ...get() }, options?.omit || []);
+
+      set(args);
+
+      // Get the current state after updating state
+      const currState = filterState({ ...get() }, options?.omit || []);
+
+      // Only store changes if state isn't equal (or option has been set)
+      const shouldStoreChange =
+        !isEqual(lastState, currState) || options?.allowUnchanged;
+
+      if (shouldStoreChange) {
+        const prevStates = getState().prevStates;
+
+        setState({
+          prevStates: [...prevStates, lastState],
+          setStore: set,
+          futureStates: [],
+          getStore: get,
+          options,
         });
-
-        // Get the last state before updating state
-        const lastState = filterState({ ...get() }, options?.omit || []);
-
-        set(args);
-
-        // Get the current state after updating state
-        const currState = filterState({ ...get() }, options?.omit || []);
-
-        // Only store changes if state isn't equal (or option has been set)
-        const shouldStoreChange =
-          !isEqual(lastState, currState) || options?.allowUnchanged;
-
-        if (shouldStoreChange) {
-          const prevStates = getState().prevStates;
-
-          setState({
-            prevStates: [...prevStates, lastState],
-            setStore: set,
-            futureStates: [],
-            getStore: get,
-            options,
-          });
-        }
-      },
-      get,
-      api
-    );
-  };
+      }
+    },
+    get,
+    api
+  );
+};
 
 export default undoMiddleware;
