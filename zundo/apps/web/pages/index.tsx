c3b4cc1c1ab63fc0b8996c6a8b59905551b52662
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-07-04 13:15:14 -0400

    configure tsup

--- zundo/apps/web/pages/index.tsx
+++ zundo/apps/web/pages/index.tsx
@@ -1,9 +1,11 @@
-import { Button } from "ui";
+import { Button } from 'ui';
+import { test } from 'zundo';
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
+      <button onClick={test}>Test</button>
       <Button />
     </div>
   );
