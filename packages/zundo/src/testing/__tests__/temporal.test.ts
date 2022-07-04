fd9058a7746c4f7552973e1251a67a0f14464ad7
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-07-04 17:54:20 -0400

    use vitetest and mocks

--- packages/zundo/src/testing/__tests__/temporal.test.ts
+++ packages/zundo/src/testing/__tests__/temporal.test.ts
@@ -1,23 +1,47 @@
+import { describe, it, expect, vi } from 'vitest';
+vi.mock('zustand/vanilla');
 import { createTemporalStore } from '../../temporal';
 import createVanilla from 'zustand/vanilla';
-import create from 'zustand';
+import { act } from 'react-dom/test-utils';
 
 interface MyState {
-  count: number
+  count: number;
+  increment: () => void;
+  decrement: () => void;
 }
 
 describe('Temporal', () => {
-  const store = createVanilla<MyState>(() => {
-    return { count: 0 };
-  })
-  const useStore = create(store);
+  const store = createVanilla<MyState>((set) => {
+    return {
+      count: 0,
+      increment: () =>
+        set((state) => ({
+          count: state.count + 1
+        })),
+      decrement: () =>   set((state) => ({
+        count: state.count - 1
+      })),
+    };
+  });
+
   it('should work', () => {
-    const temporalStore = createTemporalStore(store.setState, useStore.getState);
-    const { undo, redo, clear, pastStates, futureStates } = temporalStore.getState();
+    const temporalStore = createTemporalStore(store.setState, store.getState);
+    const { undo, redo, clear, pastStates, futureStates } =
+      temporalStore.getState();
     expect(undo).toBeDefined();
     expect(redo).toBeDefined();
     expect(clear).toBeDefined();
     expect(pastStates).toBeDefined();
     expect(futureStates).toBeDefined();
+
+    expect(store.getState().count).toBe(0);
+    act(() => {
+      store.getState().increment();
+    });
+    expect(store.getState().count).toBe(1);
+  });
+
+  it('should work', () => {
+    expect(store.getState().count).toBe(0);
   });
 });
