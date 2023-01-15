e94008d196e10b74789c38eb8dc9d430c8dea683
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-01-15 21:03:32 +0300

    uncomment selector subscriber

--- apps/web/pages/subscribeWithSelector.tsx
+++ apps/web/pages/subscribeWithSelector.tsx
@@ -1,21 +1,21 @@
-import create from "zustand";
-// import { subscribeWithSelector } from "zustand/middleware";
-// import { temporal } from "zundo";
-//
-// interface ExampleData {
-//   key1: boolean;
-//   key2: number;
-// }
+import { create } from "zustand";
+import { subscribeWithSelector } from "zustand/middleware";
+import { temporal } from "zundo";
 
-// const store = create<ExampleData>()(
-//   subscribeWithSelector(
-//     temporal(() => ({
-//       // TODO: find a way to remove this type cast.
-//       key1: false as boolean,
-//       key2: 32
-//     }))
-//   )
-// );
+interface ExampleData {
+  key1: boolean;
+  key2: number;
+}
+
+const store = create<ExampleData>()(
+  subscribeWithSelector(
+    temporal(() => ({
+      // TODO: find a way to remove this type cast.
+      key1: false as boolean,
+      key2: 32
+    }))
+  )
+);
 
 export default function Dummy() {
   return (
