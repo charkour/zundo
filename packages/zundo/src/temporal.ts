fa1d791fbbf952b76f4b1d61a0612b9238dc5f46
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2023-05-22 23:07:36 -0400

    shave 2 bytes

--- packages/zundo/src/temporal.ts
+++ packages/zundo/src/temporal.ts
@@ -28,39 +28,34 @@ export const createVanillaTemporal = <TState>(
           // Fastest way to clone an array on Chromium. Needed to create a new array reference
           const pastStates = get().pastStates.slice();
           const futureStates = get().futureStates.slice();
-          if (pastStates.length === 0) {
-            return;
-          }
-
-          // Based on the steps, get values from the pastStates array and push them to the futureStates array
-          for (let i = 0; i < steps; i++) {
-            const pastState = pastStates.pop();
-            if (pastState) {
-              futureStates.push(partialize(userGet()));
-              userSet(pastState);
+          if (pastStates.length) {
+            // Based on the steps, get values from the pastStates array and push them to the futureStates array
+            for (let i = 0; i < steps; i++) {
+              const pastState = pastStates.pop();
+              if (pastState) {
+                futureStates.push(partialize(userGet()));
+                userSet(pastState);
+              }
             }
-          }
 
-          set({ pastStates, futureStates });
+            set({ pastStates, futureStates });
+          }
         },
         redo: (steps = 1) => {
           // Fastest way to clone an array on Chromium. Needed to create a new array reference
           const pastStates = get().pastStates.slice();
           const futureStates = get().futureStates.slice();
-          if (futureStates.length === 0) {
-            return;
-          }
-
-          // Based on the steps, get values from the futureStates array and push them to the pastStates array
-          for (let i = 0; i < steps; i++) {
-            const futureState = futureStates.pop();
-            if (futureState) {
-              pastStates.push(partialize(userGet()));
-              userSet(futureState);
+          if (futureStates.length) {
+            // Based on the steps, get values from the futureStates array and push them to the pastStates array
+            for (let i = 0; i < steps; i++) {
+              const futureState = futureStates.pop();
+              if (futureState) {
+                pastStates.push(partialize(userGet()));
+                userSet(futureState);
+              }
             }
+            set({ pastStates, futureStates });
           }
-
-          set({ pastStates, futureStates });
         },
         clear: () => {
           set({ pastStates: [], futureStates: [] });
