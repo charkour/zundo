8f82d3224e1b20c1f291de15ff3e1c0dbcfd619d
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2023-05-26 19:15:55 -0400

    fix test

--- __tests__/react.test.tsx
+++ __tests__/react.test.tsx
@@ -1,7 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import Reactive from '../../../apps/web/pages/reactive';
 
 describe('React Re-renders when state changes', () => {
   it('it', () => {
@@ -44,3 +43,106 @@ describe('React Re-renders when state changes', () => {
     ).toBeTruthy();
   });
 });
+
+
+// React Code from examples/web/pages/reactive.tsx
+import { TemporalState, temporal } from '../src';
+import { StoreApi, useStore, create } from 'zustand';
+
+interface MyState {
+  bears: number;
+  increment: () => void;
+  decrement: () => void;
+}
+
+const useMyStore = create(
+  temporal<MyState>((set) => ({
+    bears: 0,
+    increment: () => set((state) => ({ bears: state.bears + 1 })),
+    decrement: () => set((state) => ({ bears: state.bears - 1 })),
+  })),
+);
+
+type ExtractState<S> = S extends {
+  getState: () => infer T;
+}
+  ? T
+  : never;
+type ReadonlyStoreApi<T> = Pick<StoreApi<T>, 'getState' | 'subscribe'>;
+type WithReact<S extends ReadonlyStoreApi<unknown>> = S & {
+  getServerState?: () => ExtractState<S>;
+};
+
+const useTemporalStore = <
+  S extends WithReact<StoreApi<TemporalState<MyState>>>,
+  U,
+>(
+  selector: (state: ExtractState<S>) => U,
+  equality?: (a: U, b: U) => boolean,
+): U => {
+  const state = useStore(useMyStore.temporal as any, selector, equality);
+  return state;
+};
+
+const HistoryBar = () => {
+  const futureStates = useTemporalStore((state) => state.futureStates);
+  const pastStates = useTemporalStore((state) => state.pastStates);
+  return (
+    <div>
+      past states: {JSON.stringify(pastStates)}
+      <br />
+      future states: {JSON.stringify(futureStates)}
+      <br />
+    </div>
+  );
+};
+
+const UndoBar = () => {
+  const { undo, redo } = useTemporalStore((state) => ({
+    undo: state.undo,
+    redo: state.redo,
+  }));
+  return (
+    <div>
+      <button onClick={() => undo()}>undo</button>
+      <button onClick={() => redo()}>redo</button>
+    </div>
+  );
+};
+
+const StateBar = () => {
+  const store = useMyStore();
+  const { bears, increment, decrement } = store;
+  return (
+    <div>
+      current state: {JSON.stringify(store)}
+      <br />
+      <br />
+      bears: {bears}
+      <br />
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+    </div>
+  );
+};
+
+const Reactive = () => {
+  return (
+    <div>
+      <h1>
+        {' '}
+        <span role="img" aria-label="bear">
+          🐻
+        </span>{' '}
+        <span role="img" aria-label="recycle">
+          ♻️
+        </span>{' '}
+        Zundo!
+      </h1>
+      <StateBar />
+      <br />
+      <UndoBar />
+      <HistoryBar />
+    </div>
+  );
+};
