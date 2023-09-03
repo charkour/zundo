79c88121d0a8f0d084761e2d29aedcdb4cd6041a
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2023-09-03 14:59:34 -0400

    bump deps (#122)

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,9 +1,6 @@
-import { expect, afterEach } from 'vitest';
+import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import matchers from '@testing-library/jest-dom/matchers';
-
-// extends Vitest's expect method with methods from react-testing-library
-expect.extend(matchers);
+import '@testing-library/jest-dom/vitest';
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
