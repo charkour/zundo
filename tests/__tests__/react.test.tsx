6f6cf3fa4dd2e5757c3c23385bad6c3253984bae
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2024-10-11 21:26:45 -0400

    tweak react test

--- tests/__tests__/react.test.tsx
+++ tests/__tests__/react.test.tsx
@@ -3,9 +3,7 @@ import { fireEvent, render } from '@testing-library/react';
 
 describe('React Re-renders when state changes', () => {
   it('it', () => {
-    const { queryByLabelText, getByLabelText, queryByText, getByText } = render(
-      <Reactive />,
-    );
+    const { queryByText, getByText } = render(<Reactive />);
 
     expect(queryByText(/bears: 0/i)).toBeTruthy();
     expect(queryByText(/increment/i)).toBeTruthy();
@@ -76,9 +74,8 @@ const useTemporalStore = <
   U,
 >(
   selector: (state: ExtractState<S>) => U,
-  equality?: (a: U, b: U) => boolean,
 ): U => {
-  const state = useStore(useMyStore.temporal as any, selector, equality);
+  const state = useStore(useMyStore.temporal as any, selector);
   return state;
 };
 
@@ -96,10 +93,8 @@ const HistoryBar = () => {
 };
 
 const UndoBar = () => {
-  const { undo, redo } = useTemporalStore((state) => ({
-    undo: state.undo,
-    redo: state.redo,
-  }));
+  const undo = useTemporalStore((state) => state.undo);
+  const redo = useTemporalStore((state) => state.redo);
   return (
     <div>
       <button onClick={() => undo()}>undo</button>
