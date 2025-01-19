eaafe8e9e6eaab5be232be6ce64330b337ad755a
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2025-01-19 13:08:41 -0500

    add structured clone polyfill for node v16 testing

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -1,3 +1,5 @@
+import structuredClone from "@ungap/structured-clone";
+
 // https://github.com/pmndrs/zustand/blob/main/docs/guides/testing.md
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
