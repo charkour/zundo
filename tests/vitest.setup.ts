c82eb1ac8728a5af50e260f8569e65a7d76b3876
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2025-01-19 13:14:52 -0500

    monkey-patch

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -1,5 +1,10 @@
 import structuredClone from "@ungap/structured-clone";
 
+if (!("structuredClone" in globalThis)) {
+    // @ts-expect-error
+    globalThis.structuredClone = structuredClone;
+  }
+
 // https://github.com/pmndrs/zustand/blob/main/docs/guides/testing.md
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
