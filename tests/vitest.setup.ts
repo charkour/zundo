bd5e01b6e9d3728f722bbb0f56e9fcfc3165398c
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2025-01-19 13:20:06 -0500

    remove structured clone polyfill

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -1,10 +1,3 @@
-import structuredClone from "@ungap/structured-clone";
-
-if (!("structuredClone" in globalThis)) {
-    // @ts-expect-error
-    globalThis.structuredClone = structuredClone;
-  }
-
 // https://github.com/pmndrs/zustand/blob/main/docs/guides/testing.md
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
